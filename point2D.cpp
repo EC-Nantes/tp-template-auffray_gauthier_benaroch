2d47a8a53e1bb1882a47a5fbb26272e98e021ac7
Yslachy <corentin.gauthier@yahoo.com>
2023-01-13 09:30:03 +0100

    Add point2D + polygone's header

---
+++ point2D.cpp
@@ -0,0 +1,26 @@
+#include "point2D.h"
+
+template<typename T>
+//Avec les templates, le nom de la classe doit tjr etre suivi de <T>
+Point2D_t<T>::Point2D_t(){
+    this->x = 0;
+    this->y = 0;
+}
+
+template<typename T>
+Point2D_t<T>::Point2D_t(T x, T y){
+    this->x = x;
+    this->y = y;
+}
+
+template<typename T>
+Point2D_t<T>::Point2D_t(Point2D_t<T>& p){
+    this->x = p->x;
+    this->y = p->y;
+}
+
+template<typename T>
+void Point2D_t<T>::translate(T x, T y){
+    this->x += x;
+    this->y += y;
+}
---
+++ polygone.h
@@ -0,0 +1,29 @@
+#ifndef POLYGONE_H
+#define POLYGONE_H
+
+#include <vector>
+#include "point2D.h"
+
+template<typename T>
+class Polygone_t {
+    private:
+        std::vector<Point2D_t<T>> sommets;
+
+    public:
+        //Constructeurs
+        Polygone_t();
+        Polygone_t(std::vector<Point2D_t<T>> listeSommets);
+        Polygone_t(Polygone_t<T>& poly);
+
+        //Getteurs
+        std::vector<Point2D_t<T>> getSommets() {return this->sommets;}
+
+        //Setteurs
+        void setSommets(std::vector<Point2D_t<T>> listeSommets) {this->sommets = listeSommets;}
+
+        //Méthodes
+        void addPoint(Point2D_t<T>&);
+        void translate(T x, T y);
+};
+
+#endif
