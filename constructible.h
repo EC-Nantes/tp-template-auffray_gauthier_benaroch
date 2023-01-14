5693edb370fa495835a09fa62aa396de1bf55ec5
AUFFRAY Jean <jean.auffray@altran.com>
2023-01-14 15:29:40 +0100

    correction double déclaration de constructible

--- constructible.h
+++ constructible.h
@@ -1,10 +1,10 @@
 #ifndef CONSTRUCTIBLE_H
-#define CONSTRUCTIblE_H
+#define CONSTRUCTIBLE_H
 
 class Constructible_t {
 public:
     Constructible_t() {}
     virtual float surfaceConstructible() const  = 0;
 };
-Constructible_t::Constructible_t() {}
+
 #endif
